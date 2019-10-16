70db0aab2ea9c5c5b18c5bea835eb79922531083
Kevin J Hou <kevin.jiayu.hou@gmail.com>
2019-10-16 15:48:21 -0700

    cleaned up nematic plotting code

--- project/models/nematic.py
+++ project/models/nematic.py
@@ -93,14 +93,15 @@ class NematicLattice(System):
         fig = plt.figure(figsize = figsize)
         ax = fig.gca(projection = '3d')
         ax.view_init(elev = 25, azim = 80)
-        ax.set_xlabel("z")
+        ax.set_xlabel("x")
         ax.set_ylabel("y")
-        ax.set_zlabel("x")
+        ax.set_zlabel("z")
 
         N = len(x)
         gx, gy, gz = np.meshgrid(np.arange(N), np.arange(N), np.arange(N))
         u, v, w = x.T
 
+        """
         # How to color 3D vectors (can alter cmap for variations)
         # Color by azimuthal angle
         c = np.arctan2(v, u) - np.arctan2(w, v)
@@ -110,9 +111,31 @@ class NematicLattice(System):
         c = np.concatenate((c, np.repeat(c, 2)))
         # Colormap
         c = plt.cm.viridis(c)
+        """
 
-        ax.quiver(gz, gy, gx, u, w, v,
+        # Coloring technique (Kevin)
+        # The idea is to assign each direction (x,y,z) a color,
+        # and assign each vector a color that is a weighted sum of these 'basis' colors.
+
+        # Pick a basis color for each direction. Must be valid RGB (less than one).
+        xcolor = np.array([0.5,0,0])
+        ycolor = np.array([0,0.5,0])
+        zcolor = np.array([0,0,0.5])
+        cmatrix = np.vstack((xcolor,ycolor,zcolor))
+ 
+        # Flatten the input
+        # The transpose is hard coded (sorry), but i don't know how to generalize it.
+        xflat = x.transpose((2,1,0,3)).reshape( (np.product(x.shape[:3]), 3)  )
+
+        # Assign a color as a weighted sum of each vector component
+        c = np.dot(np.abs(xflat),cmatrix) 
+
+        # Pad with a ones column, then stack on self three times for arrow
+        c = np.hstack( (c, np.ones( (c.shape[0],1) )) )       
+        c = np.vstack( (c, np.repeat(c,2,axis=0)) ) 
+
+        ax.quiver(gx, gy, gz, u, v, w,
             colors = c, length = 0.5, 
             alpha = alpha, pivot = 'middle',
-            normalize = True
+            normalize = True, 
         )
