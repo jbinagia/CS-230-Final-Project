ccf51284104fa8944e254187221c7eee62ffa08b
Jeremy Binagia <jbinagia@stanford.edu>
2019-10-23 10:32:39 -0700

    Implemented dataloader for crescent moon dataset (toy problem for RealNVP)

--- dl-starter-code/model/data_loader.py
+++ dl-starter-code/model/data_loader.py
@@ -4,6 +4,9 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
+import numpy as np
+from sklearn import cluster, datasets, mixture
+import torch
 
 # borrowed from http://pytorch.org/tutorials/advanced/neural_style_tutorial.html
 # and http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
@@ -56,6 +59,41 @@ class SIGNSDataset(Dataset):
         image = self.transform(image)
         return image, self.labels[idx]
 
+class CrescentDataset(Dataset):
+    """
+    A standard PyTorch definition of Dataset which defines the functions __len__ and __getitem__.
+    """
+    def __init__(self, n_points = 100, gauss_noise = 0.05):
+        """
+        Store the filenames of the jpgs to use. Specifies transforms to apply on images.
+
+        Args:
+            n_points: (int) The total number of points generated
+            noise: (double) Standard deviation of Gaussian noise added to the data.
+        """
+
+        self.xcoords = datasets.make_moons(n_samples=n_points, noise=gauss_noise)[0].astype(np.float32) # Make two interleaving half circles
+        self.labels = datasets.make_moons(n_samples=n_points, noise=gauss_noise)[1].astype(np.float32)
+
+
+    def __len__(self):
+        # return size of dataset
+        return len(self.xcoords)
+
+    def __getitem__(self, idx):
+        """
+        Fetch index idx data point and labels from dataset.
+
+        Args:
+            idx: (int) index in [0, 1, ..., size_of_dataset-1]
+
+        Returns:
+            xcoords: (double) The generated samples.
+            label: (int) The integer labels (0 or 1) for class membership of each sample.
+        """
+        return torch.from_numpy(self.xcoords[idx,:]), self.labels[idx]
+
+
 
 def fetch_dataloader(types, data_dir, params):
     """
@@ -75,15 +113,23 @@ def fetch_dataloader(types, data_dir, params):
         if split in types:
             path = os.path.join(data_dir, "{}_signs".format(split))
 
-            # use the train_transformer if training data, else use eval_transformer without random flip
+            # initialize the data loader
+                # use the train_transformer if training data, else use eval_transformer without random flip
             if split == 'train':
-                dl = DataLoader(SIGNSDataset(path, train_transformer), batch_size=params.batch_size, shuffle=True,
+                dl = DataLoader(CrescentDataset(1000, 0.05), batch_size=params.batch_size, shuffle=True,
                                         num_workers=params.num_workers,
                                         pin_memory=params.cuda)
+                # dl = DataLoader(SIGNSDataset(path, train_transformer), batch_size=params.batch_size, shuffle=True,
+                #                         num_workers=params.num_workers,
+                #                         pin_memory=params.cuda)
+                # torch.utils.data.DataLoader provides an iterator that takes in a Dataset object and performs batching, shuffling and loading of the data.
             else:
-                dl = DataLoader(SIGNSDataset(path, eval_transformer), batch_size=params.batch_size, shuffle=False,
+                dl = DataLoader(CrescentDataset(50, 0.05), batch_size=params.batch_size, shuffle=True,
                                 num_workers=params.num_workers,
                                 pin_memory=params.cuda)
+                # dl = DataLoader(SIGNSDataset(path, eval_transformer), batch_size=params.batch_size, shuffle=False,
+                #                 num_workers=params.num_workers,
+                #                 pin_memory=params.cuda)
 
             dataloaders[split] = dl
 
--- dl-starter-code/train.py
+++ dl-starter-code/train.py
@@ -15,8 +15,10 @@ import model.net as net
 import model.data_loader as data_loader
 from evaluate import evaluate
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', default='data/64x64_SIGNS',
+parser = argparse.ArgumentParser() # create a parser for command line arguments
+# parser.add_argument('--data_dir', default='data/64x64_SIGNS',
+#                     help="Directory containing the dataset")
+parser.add_argument('--data_dir', default='data/crescent',
                     help="Directory containing the dataset")
 parser.add_argument('--model_dir', default='experiments/base_model',
                     help="Directory containing params.json")
@@ -47,7 +49,7 @@ def train(model, optimizer, loss_fn, dataloader, metrics, params):
 
     # Use tqdm for progress bar
     with tqdm(total=len(dataloader)) as t:
-        for i, (train_batch, labels_batch) in enumerate(dataloader):
+        for i, (train_batch, labels_batch) in enumerate(dataloader): # for the i-th batch 
             # move to GPU if available
             if params.cuda:
                 train_batch, labels_batch = train_batch.cuda(
@@ -156,14 +158,14 @@ def train_and_evaluate(model, train_dataloader, val_dataloader, optimizer, loss_
 if __name__ == '__main__':
 
     # Load the parameters from json file
-    args = parser.parse_args()
-    json_path = os.path.join(args.model_dir, 'params.json')
+    args = parser.parse_args() # inspect the command line, convert each argument to the appropriate type and then invoke the appropriate action.
+    json_path = os.path.join(args.model_dir, 'params.json') # json file storing parameters like learning rate
     assert os.path.isfile(
         json_path), "No json configuration file found at {}".format(json_path)
-    params = utils.Params(json_path)
+    params = utils.Params(json_path) # defined in utils.py. Class that loads hyperparameters from a json file.
 
     # use GPU if available
-    params.cuda = torch.cuda.is_available()
+    params.cuda = torch.cuda.is_available() # torch.cuda.is_available() returns a bool indicating if CUDA is currently available.
 
     # Set the random seed for reproducible experiments
     torch.manual_seed(230)
@@ -171,7 +173,7 @@ if __name__ == '__main__':
         torch.cuda.manual_seed(230)
 
     # Set the logger
-    utils.set_logger(os.path.join(args.model_dir, 'train.log'))
+    utils.set_logger(os.path.join(args.model_dir, 'train.log')) # create logger that saves every output to the terminal in a permanent file
 
     # Create the input data pipeline
     logging.info("Loading the datasets...")
@@ -181,19 +183,18 @@ if __name__ == '__main__':
         ['train', 'val'], args.data_dir, params)
     train_dl = dataloaders['train']
     val_dl = dataloaders['val']
-
     logging.info("- done.")
 
-    # Define the model and optimizer
-    model = net.Net(params).cuda() if params.cuda else net.Net(params)
-    #optimizer = optim.Adam(model.parameters(), lr=params.learning_rate) # we want to exclude the mask from the update step 
-    optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad==True], lr=params.learning_rate)
-
-    # fetch loss function and metrics
-    loss_fn = net.loss_fn
-    metrics = net.metrics
-
-    # Train the model
-    logging.info("Starting training for {} epoch(s)".format(params.num_epochs))
-    train_and_evaluate(model, train_dl, val_dl, optimizer, loss_fn, metrics, params, args.model_dir,
-                       args.restore_file)
+    # # Define the model and optimizer
+    # model = net.Net(params).cuda() if params.cuda else net.Net(params) # Calling .cuda() on a model/Tensor/Variable sends it to the GPU
+    # #optimizer = optim.Adam(model.parameters(), lr=params.learning_rate) # we want to exclude the mask from the update step
+    # optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad==True], lr=params.learning_rate)
+    #
+    # # fetch loss function and metrics
+    # loss_fn = net.loss_fn
+    # metrics = net.metrics
+    #
+    # # Train the model
+    # logging.info("Starting training for {} epoch(s)".format(params.num_epochs))
+    # train_and_evaluate(model, train_dl, val_dl, optimizer, loss_fn, metrics, params, args.model_dir,
+    #                    args.restore_file)
