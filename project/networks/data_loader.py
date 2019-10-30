16884b59474d8d817b871bc74568dd2c16c9b024
Jeremy Binagia <jbinagia@stanford.edu>
2019-10-30 16:46:22 -0700

    Cleaned up projects folder (removed files based on SIGNS dataset).

--- project/networks/data_loader.py
+++ project/networks/data_loader.py
@@ -21,44 +21,6 @@ eval_transformer = transforms.Compose([
     transforms.Resize(64),  # resize the image to 64x64 (remove if images are already 64x64)
     transforms.ToTensor()])  # transform it into a torch tensor
 
-
-class SIGNSDataset(Dataset):
-    """
-    A standard PyTorch definition of Dataset which defines the functions __len__ and __getitem__.
-    """
-    def __init__(self, data_dir, transform):
-        """
-        Store the filenames of the jpgs to use. Specifies transforms to apply on images.
-
-        Args:
-            data_dir: (string) directory containing the dataset
-            transform: (torchvision.transforms) transformation to apply on image
-        """
-        self.filenames = os.listdir(data_dir)
-        self.filenames = [os.path.join(data_dir, f) for f in self.filenames if f.endswith('.jpg')]
-
-        self.labels = [int(os.path.split(filename)[-1][0]) for filename in self.filenames]
-        self.transform = transform
-
-    def __len__(self):
-        # return size of dataset
-        return len(self.filenames)
-
-    def __getitem__(self, idx):
-        """
-        Fetch index idx image and labels from dataset. Perform transforms on image.
-
-        Args:
-            idx: (int) index in [0, 1, ..., size_of_dataset-1]
-
-        Returns:
-            image: (Tensor) transformed image
-            label: (int) corresponding label of image
-        """
-        image = Image.open(self.filenames[idx])  # PIL image
-        image = self.transform(image)
-        return image, self.labels[idx]
-
 class CrescentDataset(Dataset):
     """
     A standard PyTorch definition of Dataset which defines the functions __len__ and __getitem__.
