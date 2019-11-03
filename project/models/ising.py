Fixed bug in energy calculation.

project/models/ising.py
@@ -12,7 +12,9 @@ class IsingModel(System):
 
     params_default = {
         "h" : 0.0,
-        "J" : 1.0
+        "J" : 1.0,
+        "E_high": 1000000,
+        "E_max": 10**12 # cutoff to avoid overflow
     }
 
     def __init__(self, params = None, **kwargs):
@@ -27,6 +29,8 @@ class IsingModel(System):
         return 2 * np.random.randint(0, 2, size = shape) - 1
 
     def energy(self, x):
+        assert x.shape[0] == x.shape[1] # x must be square and not flattened
+
         # Non-PBCs
         neigh = torch.zeros_like(x)
         neigh[:, :-1] += x[:, 1:]
@@ -40,9 +44,36 @@ class IsingModel(System):
         neigh[:, 0] += x[:, -1]
         neigh[:, -1] += x[:, 0]
 
-        en_field = -self.params["h"] * torch.sum(x, 1)
-        en_pair = -0.5 * self.params["J"] * torch.sum(neigh * x, 1)
+        en_field = -self.params["h"] * torch.sum(x)
+        en_pair = -0.5 * self.params["J"] * torch.sum(neigh * x)
+
+        # regularize energy
+        tmp_energy = en_field + en_pair
+        if  tmp_energy < self.params["E_high"]:
+            return tmp_energy
+        elif tmp_energy  >= self.params["E_high"] and tmp_energy < self.params["E_max"]:
+            return self.params["E_high"] + np.log(tmp_energy - self.params["E_high"] + 1)
+        elif tmp_energy > self.params["E_max"]:
+            return self.params["E_high"] + np.log(self.params["E_max"] - self.params["E_high"] + 1)
+
+    def energy_vec(self, x):
+        # Non-PBCs
+        neigh = torch.zeros_like(x)
+        neigh[:, :-1] += x[:, 1:]
+        neigh[:, 1:] += x[:, :-1]
+        neigh[:-1] += x[1:]
+        neigh[1:] += x[:-1]
+
+        # Handle PBCs
+        neigh[0, :] += x[-1, :]
+        neigh[-1, :] += x[0, :]
+        neigh[:, 0] += x[:, -1]
+        neigh[:, -1] += x[:, 0]
+
+        en_field = -self.params["h"] * torch.sum(x,1)
+        en_pair = -0.5 * self.params["J"] * torch.sum(neigh * x,1)
         return en_field + en_pair
+
 
     def energy_idx(self, x, idx):
         N = x.shape[0]

project/networks/net.py
@@ -69,20 +69,26 @@ class RealNVP(nn.Module): # base class Module
         return self.expected_value(0.5*(torch.norm(z,dim=1) - log_R_xz), batch)
 
     def loss_kl(self, batch):
-        # x, log_R_zx = self.g(z)
         log_R_zx = -self.log_R_xz
-        return self.expected_value(self.system.energy(batch) - log_R_zx, batch)
-        return 0.0
+        return self.expected_value(self.energies - log_R_zx, batch)
 
     def loss_rc(self, batch):
         return 0.0
 
     def calculate_weights(self, batch, z, log_R_xz):
+        self.energies = self.calculate_energy(batch)
         weights = batch.new_ones(batch.shape[0])
         for i in range(batch.shape[0]): # for each x in the batch
             log_prob_x = self.prior.log_prob(z[i:i+1,:]) + log_R_xz[i:i+1]
             #weights[i] = torch.exp(-self.system.energy(batch[i:i+1,:])-log_prob_x) # currently all weights are infinitely large
         return weights
+
+    def calculate_energy(self, batch):
+        energies = batch.new_zeros(batch.shape[0])
+        for i in range(batch.shape[0]): # for each x in the batch
+            config = batch[i,:].reshape(self.orig_dimension) # reshape into correct form
+            energies[i] = self.system.energy(config)
+        return energies
 
     def expected_value(self, observable, batch):
         return torch.dot(observable,self.weights)/torch.sum(self.weights)